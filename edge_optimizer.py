b99684a7267bc1a74b1dddd5eb33b620b54dd693
Michael Kato <rawmeat3000@gmail.com>
2024-06-30 19:04:28 -0700

    checkpoint

--- edge_optimizer.py
+++ edge_optimizer.py
@@ -1,19 +1,7 @@
 
 
-"""
-TODO
->> Get working in 2015
->> Refactor deleteBooleanEdges()
->> Make this into a more versitile boolean replacement tool, not just cleanup
->> Investigate creating a temp mesh that encompasses boolVerts, 
-   could check against that mesh to perform a mergeShortEdges function
-   after deleting edges and having things reordered
->> Find better way to do the mesh == om2.MFnMesh(dagPath) line
-		it's everywhee rnad it repeats a lot
-"""
-
-
-import maya.api.OpenMaya as om2
+# TODO: convert to PyMel
+import maya.api.OpenMaya as om2 
 import pymel.core as pm
 
 
@@ -157,93 +145,162 @@ class EdgeOptimizer(object):
     
 	
     def getParallelEdges(self, dagPath, vert, angleTolerance):
-        
         mesh = om2.MFnMesh(dagPath)
         
-        # Maya 2014 and 2015 don't have access to certain methods, so they need different methods
-        if int(self.mayaVersion) <= 2015:
-            faceIter = om2.MItMeshPolygon(dagPath)
-            while not faceIter.isDone():
-                faceIter.next()
-           
-        else:
-            vertIter = om2.MItMeshVertex(dagPath)
-            while not vertIter.isDone():
-                
-                if vertIter.index() == vert:
-                    connectedEdges = vertIter.getConnectedEdges()
-                    # See if any edge is parallel to any other
-                    for i, edge1 in enumerate(connectedEdges):
-                        for edge2 in connectedEdges[i+1:]:
+        vertIter = om2.MItMeshVertex(dagPath)
+        while not vertIter.isDone():
+            
+            if vertIter.index() == vert:
+                connectedEdges = vertIter.getConnectedEdges()
+                # See if any edge is parallel to any other
+                for i, edge1 in enumerate(connectedEdges):
+                    for edge2 in connectedEdges[i+1:]:
+                        
+                        edge1Vector = self.getEdgeVector(mesh, edge1)
+                        edge2Vector = self.getEdgeVector(mesh, edge2)
+                        
+                        if edge1Vector.isParallel(edge2Vector, angleTolerance):
+                            # If there are parallel edges, remove them from the list
+                            connectedEdges = self.removeFromArray(connectedEdges, edge1)
+                            connectedEdges = self.removeFromArray(connectedEdges, edge2)
                             
-                            edge1Vector = self.getEdgeVector(mesh, edge1)
-                            edge2Vector = self.getEdgeVector(mesh, edge2)
+                            # 
+                            connectedVerts = vertIter.getConnectedVertices()
                             
-                            if edge1Vector.isParallel(edge2Vector, angleTolerance):
-                                # If there are parallel edges, remove them from the list
-                                connectedEdges = self.removeFromArray(connectedEdges, edge1)
-                                connectedEdges = self.removeFromArray(connectedEdges, edge2)
-                                
-                                # 
-                                connectedVerts = vertIter.getConnectedVertices()
-                                
-                                #connectedEdges = self.checkOutlyingEdges(dagPath, connectedVerts, connectedEdges)
-                                print(connectedEdges)
-                                connectedEdges = self.checkUVBorders(self.indexToString(dagPath, connectedEdges, "e"))
-                                return connectedEdges
-                            
-                vertIter.next()
+                            #connectedEdges = self.checkOutlyingEdges(dagPath, connectedVerts, connectedEdges)
+                            print(connectedEdges)
+                            connectedEdges = self.checkUVBorders(self.indexToString(dagPath, connectedEdges, "e"))
+                            return connectedEdges
+                        
+            vertIter.next()
     
 	
     def mergeBooleanVerts(self, dagPath, boolVertPositions):
         print("Merging verts")
-        verts = om2.MFnMesh(fagPath).getPoints()
+        verts = om2.MFnMesh(dagPath).getPoints()
         
         mergeVerts = set(verts).difference(set(boolVertPositions))
-        
-        
+             
+    
+    """
+    TODO: Warn user if mesh is skinned or using vertex colors
+    """
     def deleteBooleanEdges(self, mode="Entire Mesh", angleTolerance=0.001, mergeVerts=False, mergeDistance=0.01):
         selection = om2.MGlobal.getActiveSelectionList()
         toDelete = []
 
-        if int(self.mayaVersion) <= 2015:
-            print("This doesn't work yet in 2015")
-            #This will help with older version of maya, or people who don't have service packs installed (curse them)
-            for i in range(selection.length()):
-                dagPath = selection.getDagPath(i)
-                print("Dag path", dagPath)
-                meshDag = om2.MFnDagNode(dagPath)
-                
-                # Get edges and verts to work on
-                boolVerts = self.getBooleanVertices(dagPath, mode)
-                # Iterate  over verts (unless there's an edge method)
-                for vert in boolVerts:
-                    edges = self.getParallelEdges(dagPath, vert, angleTolerance)
-                    if edges:
-                        for edge in edges:
-                            if edge not in toDelete:
-                                toDelete.append(edge)
-                    
-        else:
-            selIter = om2.MItSelectionList(selection)
-            while not selIter.isDone():
-                dagPath = selIter.getDagPath()
-                meshDag = om2.MFnDagNode(dagPath)
-                # Get edges and verts to work on
-                boolVerts = self.getBooleanVertices(dagPath, mode)
-                # Iterate  over verts (unless there's an edge method)
-                for vert in boolVerts:
-                    edges = self.getParallelEdges(dagPath, vert, angleTolerance)
-                    if edges:
-                        for edge in edges:
-                            if edge not in toDelete:
-                                toDelete.append(edge)
-                
-                selIter.next()
+        selIter = om2.MItSelectionList(selection)
+        while not selIter.isDone():
+            dagPath = selIter.getDagPath()
+            print("Dag path", dagPath)
+            meshDag = om2.MFnDagNode(dagPath)
+            # Get edges and verts to work on
+            boolVerts = self.getBooleanVertices(dagPath, mode)
+            # Iterate  over verts (unless there's an edge method)
+            for vert in boolVerts:
+                edges = self.getParallelEdges(dagPath, vert, angleTolerance)
+                if edges:
+                    for edge in edges:
+                        if edge not in toDelete:
+                            toDelete.append(edge)
+            
+            selIter.next()
         
         print("To be deleted", toDelete)
         if toDelete:
-            pm.polyDelEdge(toDelete, cv=True)         
+            pm.polyDelEdge(toDelete, cv=True)       
+
+
+    def isParallel(self, vectors, tolerance=0.0):
+        """
+        Check if a list of vectors are roughly parallel (or exactly parallel if you want).
+        """
+        for i in range(len(vectors)):
+            for j in range(i + 1, len(vectors)):
+                n1 = vectors[i]
+                n2 = vectors[j]
+                if not n1.isParallel(n2, tolerance):
+                    return False
+            
+        return True
+    
+
+    def findBrokenTangents(self, tolerance=0.0):
+        """
+        compares vertex normals and tangents for all edges of the selected mesh.
+        Returns a list of edges which aren't smoothed, but could be to reduce vertex counts
+        TODO: use more pythonic iterater: MItMeshVertex.iter()
+        """
+        selection = om2.MGlobal.getActiveSelectionList()       
+
+        for i in range(selection.length()):
+            badEdges = []
+            
+            if selection.isEmpty():
+                om2.MGlobal.displayError("No mesh selected.")
+                return
+            
+            dagPath = selection.getDagPath(i)
+            print("dag path", dagPath)
+            meshFn = om2.MFnMesh(dagPath)
+            print("meshFn", meshFn)
+            
+            edgeIter = om2.MItMeshEdge(dagPath)
+            vertIter = om2.MItMeshVertex(dagPath)
+
+            faceVertexNormals = meshFn.getNormals()
+            faceVertexTangents = meshFn.getTangents()
+            
+            # Iterate through all edges
+            while not edgeIter.isDone():
+                # skip these types of edges
+                if edgeIter.onBoundary() or edgeIter.isSmooth:
+                    edgeIter.next()
+                    continue
+
+                edgeIndex = edgeIter.index()
+                vert1 = edgeIter.vertexId(0)
+                vert2 = edgeIter.vertexId(1)
+                
+                # Get face vertex normals
+                vertIter.setIndex(vert1)
+                vert1Normals = vertIter.getNormals()
+                vertIter.setIndex(vert2)
+                vert2Normals = vertIter.getNormals()
+                
+                # Check if all normals are roughly parallel, 
+                # if not then skip this edge because we assume it's hard on purpose
+                if not self.isParallel(vert1Normals) and not self.isParallel(vert2Normals):
+                    print("skipping", edgeIndex)
+                    edgeIter.next()
+                    continue
+                
+                '''
+                # Get face vertex tangents
+                vert1Tangents = meshFn.getTangents()
+                vert2Tangents = meshFn.getTangents()
+                
+                # Skip this edge if the tangents are parallel like they should be
+                if self.isParallel(vert1Tangents) and self.isParallel(vert2Tangents):
+                    edgeIter.next()
+                    continue
+                '''
+                # this was a bad edge
+                badEdges.append(edgeIndex)
+                
+                edgeIter.next()
+
+            formattedEdges = ["{}.e[{}]".format(dagPath, edgeIndex) for edgeIndex in badEdges]
+            print(formattedEdges)
+            pm.select(formattedEdges, replace=True)
+
+
+    def getSelectedMesh(self):
+        selectedMesh = []
+        for obj in pm.selected():
+            meshNode = pm.listRelatives(obj, s=True, type="mesh")[0]
+            selectedMesh.append(meshNode)
+        return selectedMesh
 
 
 class UI(EdgeOptimizer):
@@ -252,16 +309,14 @@ class UI(EdgeOptimizer):
         windowName = "EdgeOptimizer"
     
         if pm.window(windowName, exists=True):
+            print("Deleting window:", pm.window(windowName, query=True, title=True))
             pm.deleteUI(windowName)
         
-        pm.window(windowName, t=windowName, menuBar=True)
+        pm.window(windowName, t=windowName, menuBar=True, toolbox=True)
         
         pm.menuBarLayout()
         pm.menu( label='Help' )
         pm.menuItem( label='Get help!', command=lambda *args:self.helpWindow() )
-        #pm.menuItem( label='Polycount Thread' )
-        #pm.menuItem( label='Creative Crash' )
-        #pm.menuItem( label='My Website' )
         
         # Padding to make things look nicer
         pm.frameLayout(labelVisible=False, marginHeight=10, marginWidth=10)
@@ -278,15 +333,21 @@ class UI(EdgeOptimizer):
         pm.menuItem(label='Entire Mesh')
         pm.menuItem(label='Boolean History')
         pm.menuItem(label='By Material')
-        self.mergeVertsCheckbox = pm.checkBoxGrp(label="Merge verts afterward", columnAlign=[1,"left"], columnAttach=[2,"left", -10], changeCommand=lambda *args:self.toggleVertMergeSlider())
         pm.setParent('..')
-        self.mergeDistSlider = pm.floatSliderGrp(l="Vert Merge Dist", field=True, value=0.05, 
+        self.mergeVertsCheckbox = pm.checkBoxGrp(label="Merge verts afterward", columnAlign=[1,"left"], columnAttach=[2,"left", -10], changeCommand=lambda *args:self.toggleVertMergeSlider())
+        self.mergeDistSlider = pm.floatSliderGrp(l="Vert Merge Dist", field=True, value=0.01, step=0.001, 
                                         columnWidth=([2,0], [3,150]), adjustableColumn=3, columnAttach3=["right","left","right"], 
                                         columnOffset3=[40,-40,0], annotation="Info text", visible=False)
-        
         pm.setParent('..')
 
         pm.button(l="Clean it!", h=40, c=lambda *args:self.buttonPress(), bgc=[0.6,0.8,0.6])
+
+        pm.separator()
+
+        pm.columnLayout(rowSpacing=10, adjustableColumn=True)
+        pm.button(l="Find Smoothable Hard Edges", h=40, c=lambda *args:self.findBrokenTangents(), bgc=[0.6,0.7,0.6])
+        self.smoothEdgesAfter = pm.checkBoxGrp(label="Fix After Finding", columnAlign=[1,"left"], columnAttach=[2,"left", -10])
+        pm.setParent('..')
         
         pm.showWindow(windowName)
         
@@ -307,8 +368,9 @@ class UI(EdgeOptimizer):
         
         pm.window(windowName, t=windowName)
         pm.columnLayout(rowSpacing=10, adjustableColumn=True)
-        pm.text(l="This tool I made in 2015 at work, then recreated at home from scratch. \r\nIt is meant to remove superfluous edges from a model which\
-        \r\ndo not add detail and can safely be optimized away without affecting the visual look.\r\n")
+        pm.text(l="\r\nThis tool is meant to remove superfluous edges from a model which\
+        \r\ndo not add detail and can safely be optimized away without affecting the visual look.\
+        \r\nit can also detect edges that can be smoothed without affecting the look of the model to reduce vertex counts")
         pm.showWindow(windowName)
 		
 
